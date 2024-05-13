0b3b2b36c2d1931b6b0de2cf1c0df9545104f1e3
Mzzqq <rezarifa661@gmail.com>
2024-05-13 08:22:28 +0700

    refactor: move the entire php code to process_nilai.php

--- pertemuan-6/process_nilai.php
+++ pertemuan-6/process_nilai.php
@@ -1,1 +1,32 @@
 <?php
+if (isset($_POST['proses-button'])) {
+    $npm = $_POST['npm']; // mengarah ke name yang ada di tag input html
+    $nilai = $_POST['nilai'];
+    $repeat = $_POST['repeat'];
+//      echo $npm . " " . $nilai;
+    $huruf_mutu = "";
+
+
+    if (isset($nilai)) {
+        if ($nilai >= 85) {
+            $huruf_mutu = "A";
+        } elseif ($nilai >= 75) {
+            $huruf_mutu = "B";
+        } elseif ($nilai >= 65) {
+            $huruf_mutu = "C";
+        } else {
+            $huruf_mutu = "E";
+        }
+//        echo "Nilai mata kuliah anda adalah " . $huruf_mutu;
+    }
+
+    for ($i = 0; $i < $repeat; $i++) {
+        echo " <script>alert('" . $npm . "Nilai mata kuliah anda adalah: " . $huruf_mutu . "')</script>";
+    }
+
+//        header("Location: " . $_SERVER['PHP_SELF']);
+//        exit();
+
+}
+
+?>
