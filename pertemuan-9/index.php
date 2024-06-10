28576178a0534dfee09c64b9b3da87a30db046a8
Mzzqq <rezarifa661@gmail.com>
2024-06-10 10:26:35 +0700

    feat: complete features

--- pertemuan-9/index.php
+++ pertemuan-9/index.php
@@ -8,21 +8,52 @@
     <title>6th</title>
 </head>
 <body>
-<form action="process_nilai.php" method="POST">
-    <label>
-        NPM <input type="number" name="npm" value="" />
-    </label>
-    <br>
-    <label>
-        Nilai <input type="number" name="nilai" value="" />
-    </label>
-    <br>
-    <label>
-        Ulangi <input type="number" name="repeat" value="" />
-    </label>
-    <br>
-    <button type="submit" name="proses-button">Proses Data</button>
-</form>
+
+<div class="span9" id="content">
+    <!-- morris stacked chart -->
+    <div class="row-fluid">
+        <!-- block -->
+        <div class="block">
+            <div class="navbar navbar-inner block-header">
+                <div class="muted pull-left">Input Nilai Mahasiswa</div>
+            </div>
+            <div class="block-content collapse in">
+                <div class="span12">
+                    <form action="process_nilai.php" method="POST" class="form-horizontal">
+                        <fieldset>
+                            <legend>Input Nilai Mahasiswa</legend>
+                            <div class="control-group">
+                                <label class="control-label" for="npm">NPM MAHASISWA</label>
+                                <div class="controls">
+                                    <input type="number" name="npm" value="" class="input-xlarge focused" id="npm"/>
+                                </div>
+                            </div>
+
+                            <div class="control-group">
+                                <label class="control-label" for="nilai">NILAI MAHASISWA</label>
+                                <div class="controls">
+                                    <input type="number" name="nilai" value="" class="input-xlarge focused" id="nilai"/>
+                                </div>
+                            </div>
+
+                            <div class="control-group">
+                                <label class="control-label" for="repeat">ULANGI</label>
+                                <div class="controls">
+                                    <input type="number" name="repeat" value="" class="input-xlarge focused" id="repeat"/>
+                                </div>
+                            </div>
+
+                            <div class="form-actions">
+                                <button type="submit" class="btn btn-primary" name="proses-button">PROSES DATA</button>
+                                <button type="reset" class="btn">Cancel</button>
+                            </div>
+<!--                        </fieldset>-->
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
