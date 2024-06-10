219e51f0ce61cab75d227015bf993dc0e4cc7253
Mzzqq <rezarifa661@gmail.com>
2024-06-10 10:27:07 +0700

    Initialize commit

---
+++ pertemuan-10/process_nilai.php
@@ -0,0 +1,33 @@
+<?php
+//if (isset($_POST['proses-button'])) {
+    $npm = $_POST['npm']; // mengarah ke name yang ada di tag input html
+    $nilai = $_POST['nilai'];
+    $repeat = $_POST['repeat'];
+//      echo $npm . " " . $nilai;
+    $huruf_mutu = "";
+
+
+    if (isset($nilai)) {
+        if ($nilai >= 85) {
+            $huruf_mutu = "A";
+        } elseif ($nilai >= 75) {
+            $huruf_mutu = "B";
+        } elseif ($nilai >= 65) {
+            $huruf_mutu = "C";
+        } else {
+            $huruf_mutu = "E";
+        }
+//        echo "Nilai mata kuliah anda adalah " . $huruf_mutu;
+        echo $npm . " Nilai mata kuliah anda adalah: " . $huruf_mutu . "<br>" ;
+    }
+
+    for ($i = 0; $i < $repeat; $i++) {
+        echo $npm . " Nilai mata kuliah anda adalah: " . $huruf_mutu . "<br>" ;
+    }
+
+
+//        header("Location: " . $_SERVER['PHP_SELF']);
+//        exit();
+
+//}
+
