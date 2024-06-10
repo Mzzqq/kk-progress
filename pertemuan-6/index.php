e4f7d182b9d4b936f3f60583f957b10c620b8ee0
Mzzqq <rezarifa661@gmail.com>
2024-06-10 09:51:07 +0700

    feat(pertemuan-6): complete features

--- pertemuan-6/index.php
+++ pertemuan-6/index.php
@@ -7,20 +7,13 @@ echo $var1;
 
 <html>
 <header>
-    <title>Pertemuan 7</title>
+    <title>Pertemuan 6</title>
 </header>
 <body>
 <form action="" method="get">
-    <label for="inputan_pertama">NPM</label>
-    <input type="text" name="inputan_pertama" value="">
-    <br>
-    <label for="inputan_pertama">NILAI</label>
-    <input type="text" name="nilai" value="">
-    <br>
-    <label for="inputan_pertama">ULANGI</label>
-    <input type="number" name="ulangi" value="">
-    <br>
-    <button type="submit" name="process_data">PROSES DATA</button>
+    <input type="text" name="inputan_pertama">
+    <button type="submit" name="proses_data">PROSES DATA</button>
+</form>
 </form>
 </body>
 </html>
