e500895da60bdb649b91ba59bda3798dd3b1850f
Mzzqq <rezarifa661@gmail.com>
2024-06-16 09:17:57 +0700

    feat(pertemuan-10): add pop-up if data was saved or not

--- pertemuan-10/process_nilai.php
+++ pertemuan-10/process_nilai.php
@@ -2,39 +2,40 @@
 
 include "koneksi.php";
 
-//if (isset($_POST['proses-button'])) {
-    $npm = $_POST['npm']; // mengarah ke name yang ada di tag input html
-    $nilai = $_POST['nilai'];
-    $repeat = $_POST['repeat'];
-//      echo $npm . " " . $nilai;
-    $huruf_mutu = "";
+    $nama_mhs = $_POST['nama'];
+    $prodi_mhs = $_POST['prodi'];
+    $repeat = $_POST['ulangi'];
 
-    $query = mysqli_query($koneksi, "INSERT INTO mahasiswa (nama_mahasiswa, prodi) 
+    $query = mysqli_query($db, "INSERT INTO mahasiswa (nama_mahasiswa, prodi) 
                 VALUES ('$nama_mhs','$prodi_mhs') ")
-    or die(mysqli_error($koneksi));
+    or die(mysqli_error($db));
+
+    if($query){
+    echo"<script>alert('Data Berhasil Disimpan')</script>";
+} else echo"<script>alert('Data Gagal Disimpan')</script>";
 
 
-    if (isset($nilai)) {
-        if ($nilai >= 85) {
-            $huruf_mutu = "A";
-        } elseif ($nilai >= 75) {
-            $huruf_mutu = "B";
-        } elseif ($nilai >= 65) {
-            $huruf_mutu = "C";
-        } else {
-            $huruf_mutu = "E";
-        }
-//        echo "Nilai mata kuliah anda adalah " . $huruf_mutu;
+//    if (isset($nilai)) {
+//        if ($nilai >= 85) {
+//            $huruf_mutu = "A";
+//        } elseif ($nilai >= 75) {
+//            $huruf_mutu = "B";
+//        } elseif ($nilai >= 65) {
+//            $huruf_mutu = "C";
+//        } else {
+//            $huruf_mutu = "E";
+//        }
+////        echo "Nilai mata kuliah anda adalah " . $huruf_mutu;
+////        echo $npm . " Nilai mata kuliah anda adalah: " . $huruf_mutu . "<br>" ;
+//    }
+//
+//    for ($i = 0; $i < $repeat; $i++) {
 //        echo $npm . " Nilai mata kuliah anda adalah: " . $huruf_mutu . "<br>" ;
-    }
-
-    for ($i = 0; $i < $repeat; $i++) {
-        echo $npm . " Nilai mata kuliah anda adalah: " . $huruf_mutu . "<br>" ;
-    }
-
-
-//        header("Location: " . $_SERVER['PHP_SELF']);
-//        exit();
-
-//}
+//    }
+//
+//
+////        header("Location: " . $_SERVER['PHP_SELF']);
+////        exit();
+//
+////}
 
