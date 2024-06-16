d3f20fb54d1901aa169c1b07553c65033e1e68d7
Mzzqq <rezarifa661@gmail.com>
2024-06-16 09:36:25 +0700

    feat(pertemuan-12): finalize hapus_data.php

---
+++ pertemuan-12/hapus_data.php
@@ -0,0 +1,10 @@
+<?php
+    include "koneksi.php";
+
+    $npm = $_GET['id'];
+
+    $query = mysqli_query($db, "DELETE FROM mahasiswa WHERE id = $npm") or die(mysqli_error($db));
+
+    if($query){
+        echo"<script>alert('Data Berhasil Dihapus')window.location.href='index.php'</script>";
+    } else echo"<script>alert('Data Gagal Dihapus')window.location.href='index.php'</script>";
