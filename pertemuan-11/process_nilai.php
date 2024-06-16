b3582f40a8b83e2013ab7e7d2491de4a68cb84de
Mzzqq <rezarifa661@gmail.com>
2024-06-16 09:48:45 +0700

    fix(pertemuan-11): fix pop up window doesnt show when save new data

--- pertemuan-11/process_nilai.php
+++ pertemuan-11/process_nilai.php
@@ -1,6 +1,6 @@
 <?php
 
-include "koneksi.php";
+    include "koneksi.php";
 
     $nama_mhs = $_POST['nama'];
     $prodi_mhs = $_POST['prodi'];
@@ -11,10 +11,16 @@ include "koneksi.php";
     or die(mysqli_error($db));
 
     if($db){
-        echo"<script>alert('Data Berhasil Disimpan')window.location.href='index.php'</script>";
-    } else {
-        echo"<script>alert('Data Gagal Disimpan')window.location.href='index.php'</script>";
-    }
+        echo"
+            <script>
+                alert('Data Berhasil Dihapus')
+                window.location.href='index.php'
+            </script>";
+    } else echo"
+            <script>
+                alert('Data Gagal Dihapus')
+                window.location.href='index.php'
+            </script>";
 
 
 //    if (isset($nilai)) {
