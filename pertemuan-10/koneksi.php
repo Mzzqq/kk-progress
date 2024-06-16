806fb4f459bc12e8b1e1794de669bc03126ad44b
Mzzqq <rezarifa661@gmail.com>
2024-06-16 09:19:09 +0700

    refactor(pertemuan-10): change db name

--- pertemuan-10/koneksi.php
+++ pertemuan-10/koneksi.php
@@ -1,3 +1,3 @@
 <?php
 
-$db = mysqli_connect("localhost", "root", "", "kuliah_pemrograman_web") or die("Koneksi gagal");
+$db = mysqli_connect("localhost", "root", "", "kuliah_pemograman_web") or die("Koneksi gagal");
