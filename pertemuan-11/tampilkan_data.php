5aa53f47d19308dee153101bf4b325df85ca0ae4
Mzzqq <rezarifa661@gmail.com>
2024-06-16 09:43:48 +0700

    feat(pertemuan-11): fix variable name at tampilkan_data.php

--- pertemuan-11/tampilkan_data.php
+++ pertemuan-11/tampilkan_data.php
@@ -1,4 +1,4 @@
 <?php
     include "koneksi.php";
 
-    $proses = mysqli_query($db, "SELECT * FROM mahasiswa") or die(mysqli_error($db));
+    $query = mysqli_query($db, "SELECT * FROM mahasiswa") or die(mysqli_error($db));
