8e885cb0e4df7e979f052df80df8aff1f1a84a67
Mzzqq <rezarifa661@gmail.com>
2024-06-16 10:00:14 +0700

    feat(pertemuan-13): add edit feature

--- pertemuan-13/index.php
+++ pertemuan-13/index.php
@@ -14,90 +14,83 @@
     <title>6th</title>
 </head>
 <body>
-
-
 <div class="span9" id="content">
-    <!-- morris stacked chart -->
     <div class="row-fluid">
-        <!-- block -->
         <div class="block">
             <div class="navbar navbar-inner block-header">
                 <div class="muted pull-left">Input Nilai Mahasiswa</div>
             </div>
             <div class="block-content collapse in">
                 <div class="span12">
-                    <form action="process_nilai.php" method="POST">
+                    <form action="<?php echo isset($data_edit['id']) ? 'edit_data.php?id=' . $data_edit['id'] . '&proses=1' : 'process_nilai.php'; ?>" method="POST">
                         <fieldset>
                             <legend>Input Nilai Mahasiswa</legend>
 
                             <div class="control-group">
-                                <label class="control-label" for="NPM">NAMA MAHASISWA</label>
+                                <label class="control-label" for="nama">NAMA MAHASISWA</label>
                                 <div class="controls">
-                                    <input type="text" class="input-xlarge focused" id="NPM" name="nama" value="">
+                                    <input type="text" class="input-xlarge focused" id="nama" name="nama" value="<?php echo isset($data_edit['nama_mahasiswa']) ? $data_edit['nama_mahasiswa'] : ''; ?>">
                                 </div>
+                            </div>
 
-                                <div class="control-group">
-                                    <label class="control-label" for="NPM">PRODI MAHASISWA</label>
-                                    <div class="controls">
-                                        <input type="text" class="input-xlarge focused" id="npm" name="prodi" value="">
-                                    </div>
+                            <div class="control-group">
+                                <label class="control-label" for="prodi">PRODI MAHASISWA</label>
+                                <div class="controls">
+                                    <input type="text" class="input-xlarge focused" id="prodi" name="prodi" value="<?php echo isset($data_edit['prodi']) ? $data_edit['prodi'] : ''; ?>">
+                                </div>
+                            </div>
 
-                                    <div class="control-group">
-                                        <label class="control-label" for="NPM">ULANGI</label>
-                                        <div class="controls">
-                                            <input type="text" class="input-xlarge focused" id="NPM" name="ulangi" value="">
-                                        </div>
-
-                                        <div class="form-actions">
-                                            <button type="submit" class="btn btn-primary">SUBMIT</button>
-                                            <button type="reset" class="btn">Cancel</button>
-                                        </div>
+                            <div class="form-actions">
+                                <button type="submit" class="btn btn-primary">SUBMIT</button>
+                                <button type="reset" class="btn">Cancel</button>
+                            </div>
+                        </fieldset>
                     </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="row-fluid">
+        <div class="block">
+            <div class="navbar navbar-inner block-header">
+                <div class="muted pull-left">Data Mahasiswa</div>
+            </div>
+            <div class="block-content collapse in">
+                <div class="span12">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>NPM Mahasiswa</th>
+                            <th>Nama Mahasiswa</th>
+                            <th>Prodi Mahasiswa</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $result = mysqli_query($db, "SELECT * FROM mahasiswa");
+                        while ($data = mysqli_fetch_assoc($result)) {
+                            ?>
+                            <tr>
+                                <td><?php echo $data['id'] ?></td>
+                                <td><?php echo $data['nama_mahasiswa'] ?></td>
+                                <td><?php echo $data['prodi'] ?></td>
+                                <td>
+                                    <a href="index.php?id=<?php echo $data['id']; ?>">Edit</a> |
+                                    <a href="hapus_data.php?id=<?php echo $data['id']; ?>">Hapus</a>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="row-fluid">
-    <!-- block -->
-    <div class="block">
-        <div class="navbar navbar-inner block-header">
-            <div class="muted pull-left">Data Mahasiswa</div>
-        </div>
-        <div class="block-content collapse in">
-            <div class="span12">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>NPM Mahasiswa</th>
-                        <th>Nama Mahasiswa</th>
-                        <th>Prodi Mahasiswa</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    <?php
-                    while($data = mysqli_fetch_assoc($query)) {
-                        ?>
-
-                        <tr>
-                            <td><?php echo $data['id'] ?></td>
-                            <td><?php echo $data['nama_mahasiswa'] ?></td>
-                            <td><?php echo $data['prodi'] ?></td>
-                            <td>Edit | <a href="hapus_data.php?id=<?php echo $data['id']; ?>".>Hapus</a></td>
-                        </tr>
-                        <?php
-                    }
-                    ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <!-- /block -->
-</div>
 </body>
 </html>
